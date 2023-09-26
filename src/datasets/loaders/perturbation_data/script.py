3068b4ecf2f9172c3ec35739618ef9ebd2411997
Daniel Strobl <danielstrobl@gmail.com>
2023-09-26 17:39:19 +0200

    loader

--- src/datasets/loaders/perturbation_data/script.py
+++ src/datasets/loaders/perturbation_data/script.py
@@ -4,11 +4,14 @@ import pandas as pd
 import anndata as ad
 import utils
 
+par = {"output": "output.h5ad"}
+
 
 def optical_screen(csv_path):
     csv = pd.read_csv(csv_path, index_col=0)
     layers = utils.pivot(csv, 'gene', 'Cell line', ['IL1b_defect', 'TNFa_defect'])
     adata = ad.AnnData(X=next(iter(layers.values())), layers=layers)
     adata.uns['layers'] = list(layers.keys())
-    adata.write_h5ad('optical_screen.h5ad', compression="gzip")
+    adata.write_h5ad(par["output"], compression="gzip")
 
+optical_screen(PATH)
